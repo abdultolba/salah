0bde62f27f2a8942e5f70864994a35e872756b5e
Abdulrahman Tolba <abetolba@gmail.com>
2019-06-17 09:54:35 -0400

    Refactoring, code documentation

--- src/components/App.js
+++ src/components/App.js
@@ -13,17 +13,26 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      latitude: null,
-      longitude: null,
-      elevation: null,
+      location: {
+        latitude: null,
+        longitude: null,
+        elevation: null
+      },
       address: "",
       prayerTimes: [],
-      errorMessage: "",
       loading: true,
       manualForm: false
     };
   }
 
+  /**
+   * Purpose: Send a GET request using axios to fetch the prayer times
+   *          based on the current location. To change the calculation,
+   *          change the 'school' parameter (refer to API docs).
+   * @param lat Latitude
+   * @param long Longitude
+   * @param elevation The elevation retrieved from our geolocation
+   */
   fetchData = async (lat, long, elevation) => {
     const response = await prayertimes.get("/times/today.json", {
       params: {
@@ -33,61 +42,86 @@ class App extends Component {
         school: 2
       }
     });
-    return response;
+    this.setState({
+      location: {
+        latitude: lat,
+        longitude: long,
+        elevation: elevation
+      },
+      prayerTimes: Object.entries(response.data.results.datetime[0].times),
+      loading: false
+    });
   };
 
+  /**
+   * Purpose: Display a text input field to alloww the user to search
+   *          manually for their location.
+   * @param event The input event (onClick)
+   */
   handleButtonClick = event => {
     event.preventDefault();
     this.setState({ manualForm: true });
   };
 
+  /**
+   * Purpose: Set the location in our state based on the location
+   *          selected manually by the user.
+   * @param lat Latitude
+   * @param long Longitude
+   */
   handleMapsSearch = (lat, long) => {
-    this.setState({ latitude: lat, longitude: long });
+    this.setState({
+      location: {
+        latitude: lat,
+        longitude: long,
+        elevation: this.state.elevation
+      }
+    });
   };
 
+  /**
+   * Purpose: Reverse Geocode a users address using their geolocation
+   *          - Refer to Google Maps API for more information
+   */
   setAddress = () => {
     Geocode.setApiKey("AIzaSyC8t1G9mouMmq1JxJfYZTaGei8j_C-kDK4");
     Geocode.fromLatLng(
-      `${this.state.latitude}`,
-      `${this.state.longitude}`
+      `${this.state.location.latitude}`,
+      `${this.state.location.longitude}`
     ).then(
       response => {
         const address = response.results[0].formatted_address;
         this.setState({ address });
       },
-      error => {
-        console.error(error);
-      }
+      error => console.error(error)
     );
   };
 
+  /**
+   * Purpose: Hide the input field once a location is selected
+   */
   unMountInputField = () => {
-    this.setState({ manualForm: false});
-  }
+    this.setState({ manualForm: false });
+  };
 
+  /**
+   * Purpose: Attempt to retrieve the users geolocationa and send
+   *          the GET requests once the component has mounted.
+   */
   componentDidMount = () => {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(
         async position => {
-          const response = await this.fetchData(
+          await this.fetchData(
             position.coords.latitude,
             position.coords.longitude,
             position.coords.altitude
           );
-          this.setState({
-            latitude: position.coords.latitude,
-            longitude: position.coords.longitude,
-            elevation: position.coords.altitude,
-            prayerTimes: Object.entries(
-              response.data.results.datetime[0].times
-            ),
-            loading: false
-          });
           this.setAddress();
         },
         err => {
-          alert('Could not get your location. Please provide it manually');
-          this.setState({ errorMessage: err.message, loading: false })
+          alert("Could not get your location. Please provide it manually.");
+          this.setState({ loading: false });
         },
         {
           enableHighAccuracy: true,
@@ -96,23 +130,23 @@ class App extends Component {
         }
       );
     } else {
-      alert('Please ensure your browser\'s location services are enabled.')
-      console.log("Navigator not supported.");
-      this.setState({loading: false});
+      alert("Please ensure your browser's location services are enabled.");
+      this.setState({ loading: false });
     }
   };
 
+  /**
+   * Purpose: If the location changes, re-fetch the data and rerender
+   * @param prevProps The previous component props
+   * @param prevState THe previous state used to check if changes were made
+   */
   componentDidUpdate = async (prevProps, prevState) => {
-    if (this.state.latitude !== prevState.latitude) {
-      const response = await this.fetchData(
-        this.state.latitude,
-        this.state.longitude,
-        this.state.elevation
+    if (this.state.location.latitude !== prevState.location.latitude) {
+      await this.fetchData(
+        this.state.location.latitude,
+        this.state.location.longitude,
+        this.state.location.elevation
       );
-      this.setState({
-        prayerTimes: Object.entries(response.data.results.datetime[0].times),
-        loading: false
-      });
       this.setAddress();
     }
   };
